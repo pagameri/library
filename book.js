4dbc8b3d19529f01af0a60d22c6c96dd59d08179
Gabi <pagameri@gmail.com>
2022-09-02 00:07:24 +0100

    Fix createTable() for Year

--- book.js
+++ book.js
@@ -1,47 +1,54 @@
 let myLibrary = [
-  // {
-  //   title: 'Harry Potter and the Philosopher\'s Stone',
-  //   author: 'J.K. Rowling',
-  //   pages: '352',
-  //   read: 'yes',
-  // },
-  // {
-  //   title: 'One: Simple One-Pan Wonders',
-  //   author: 'Jamie Oliver',
-  //   pages: '312',
-  //   read: 'no',
-  // },
-  // {
-  //   title: 'The Boy, The Mole, The Fox and The Horse',
-  //   author: 'The Boy, The Mole, The Fox and The Horse',
-  //   pages: '128',
-  //   read: 'no',
-  // },
-  // {
-  //   title: 'The Da Vinci Code',
-  //   author: 'Dan Brown',
-  //   pages: '416',
-  //   read: 'yes',
-  // },
-  // {
-  //   title: 'Angels and Demons',
-  //   author: 'Dan Brown',
-  //   pages: '572',
-  //   read: 'yes',
-  // },
-  // {
-  //   title: 'The Ink Black Heart',
-  //   author: 'Robert Galbraith',
-  //   pages: '1024',
-  //   read: 'no',
-  // },
+  {
+    title: 'Harry Potter and the Philosopher\'s Stone',
+    author: 'J.K. Rowling',
+    pages: '352',
+    year: 'xxxx',
+    read: 'yes',
+  },
+  {
+    title: 'One: Simple One-Pan Wonders',
+    author: 'Jamie Oliver',
+    pages: '312',
+    year: 'xxxx',
+    read: 'no',
+  },
+  {
+    title: 'The Boy, The Mole, The Fox and The Horse',
+    author: 'The Boy, The Mole, The Fox and The Horse',
+    pages: '128',
+    year: 'xxxx',
+    read: 'no',
+  },
+  {
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    pages: '416',
+    year: 'xxxx',
+    read: 'yes',
+  },
+  {
+    title: 'Angels and Demons',
+    author: 'Dan Brown',
+    pages: '572',
+    year: 'xxxx',
+    read: 'yes',
+  },
+  {
+    title: 'The Ink Black Heart',
+    author: 'Robert Galbraith',
+    pages: '1024',
+    year: 'xxxx',
+    read: 'no',
+  },
 ];
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+function Book(title, author, pages, year, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.year = year;
+  this.read = read;
 }
 
 function createTableHead(table, headings) {
@@ -89,12 +96,21 @@ const addBook = document.getElementById('add-book');
 const titleInput = document.getElementById('title');
 const authorInput =  document.getElementById('author');
 const pagesInput = document.getElementById('pages');
+const yearInput = document.getElementById('year');
 const readChkBox = document.getElementById('read');
+const form = document.querySelector('form')
+
+if (myLibrary.length > 0) {
+  let headings = Object.keys(myLibrary[0]);
+  createTable(table, myLibrary)
+  createTableHead(table, headings);
+}
 
 function resetForm(...inputs) {
   titleInput.value = "";
   authorInput.value = "";
   pagesInput.value = "";
+  yearInput.value = "";
   readChkBox.checked = false;
 }
 
@@ -103,6 +119,7 @@ function addBookToLibrary() {
     title: titleInput.value,
     author: authorInput.value,
     pages: pagesInput.value,
+    year: yearInput.value,
     read: isRead(readChkBox),
   }
 
@@ -110,7 +127,7 @@ function addBookToLibrary() {
 }
 
 newBook.addEventListener('click', e => {
-  // show form (originally hidden)
+  form.style.display = "block";
 });
 
 addBook.addEventListener('click', e => {
@@ -124,6 +141,7 @@ addBook.addEventListener('click', e => {
     addToTable(table, myLibrary[myLibrary.length - 1]);
   }
 
-  resetForm(titleInput, authorInput, pagesInput, readChkBox);
+  resetForm(titleInput, authorInput, pagesInput, yearInput, readChkBox);
+  form.style.display = "none";
 });
 
