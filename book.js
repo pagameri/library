f2fb7afd44c61b8eaf2b718c71cc84eb37e9271f
Gabi <pagameri@gmail.com>
2022-09-03 00:30:19 +0100

    Fix issues around adding delete button
    To be able to delete the books we intend, I had to add data attributes.
    I also needed a counter on the book objects to have a corresponding
    match. Fixed the issue up until the point where deleting the last book
    could cause a problem (this is to be tackled next).
    
    Besides adding a counter I also had to solve the issue of printing the
    counter while looping through the object keys and values at table
    creation.
    
    Then I changed the order of key-value pairs: put the counter as the
    first property of the object. This way that can easily be removed from
    the beginning of the list as adding it to the end could cause issues in
    the future if further properties were added after that.
    
    Furthermore, I moved the addToTable() inside the createTable() to cut
    out repetition for ease of maintenance.

--- book.js
+++ book.js
@@ -1,5 +1,6 @@
 let myLibrary = [
   {
+    count: '0',
     title: 'Harry Potter and the Philosopher\'s Stone',
     author: 'J.K. Rowling',
     pages: '352',
@@ -7,6 +8,7 @@ let myLibrary = [
     read: 'yes',
   },
   {
+    count: '1',
     title: 'One: Simple One-Pan Wonders',
     author: 'Jamie Oliver',
     pages: '312',
@@ -14,6 +16,7 @@ let myLibrary = [
     read: 'no',
   },
   {
+    count: '2',
     title: 'The Boy, The Mole, The Fox and The Horse',
     author: 'The Boy, The Mole, The Fox and The Horse',
     pages: '128',
@@ -21,6 +24,7 @@ let myLibrary = [
     read: 'no',
   },
   {
+    count: '3',
     title: 'The Da Vinci Code',
     author: 'Dan Brown',
     pages: '416',
@@ -28,6 +32,7 @@ let myLibrary = [
     read: 'yes',
   },
   {
+    count: '4',
     title: 'Angels and Demons',
     author: 'Dan Brown',
     pages: '572',
@@ -35,6 +40,7 @@ let myLibrary = [
     read: 'yes',
   },
   {
+    count: '5',
     title: 'The Ink Black Heart',
     author: 'Robert Galbraith',
     pages: '1024',
@@ -49,37 +55,58 @@ function Book(title, author, pages, year, read) {
   this.pages = pages;
   this.year = year;
   this.read = read;
+  this.count = count;
+}
+
+let dataCount = 0;
+
+function createDelBtn(row) {
+  let cell = row.insertCell();
+  let delBtn = document.createElement('button');
+  delBtn.innerHTML = "Delete";
+  delBtn.setAttribute('class', 'delete-book');
+  cell.appendChild(delBtn);
+}
+
+function addDataSet(row) {
+  data = row.dataset;
+  data.dataCount = dataCount + 1;
+  dataCount++;
 }
 
 function createTableHead(table, headings) {
   let thead = table.createTHead();
   let row = thead.insertRow();
-  for (let heading of headings) {
+  for (let i = 0; i < headings.length; i++) {
     let th = document.createElement('th');
-    let text = document.createTextNode(heading.charAt(0).toUpperCase() + heading.slice(1));
+    let text = document.createTextNode(headings[i].charAt(0).toUpperCase() + headings[i].slice(1));
     th.appendChild(text);
     row.appendChild(th);
   }
+  let th = document.createElement('th');
+  let text = document.createTextNode('Delete');
+  th.appendChild(text);
+  row.appendChild(th);
 }
 
 function createTable(table, array) {
   for (let element of array) {
-    let row = table.insertRow();
-    for (key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
+    addToTable(table, element);
   }
 }
 
 function addToTable(table, object) {
   let row = table.insertRow();
-  for (key in object) {
+  let values = Object.values(object);
+  values.shift();
+
+  for (i = 0; i < values.length; i++) {
     let cell = row.insertCell();
-    let text = document.createTextNode(object[key]);
+    let text = document.createTextNode(values[i]);
     cell.appendChild(text);
   }
+  createDelBtn(row);
+  addDataSet(row);
 }
 
 function isRead(checkbox) {
@@ -98,10 +125,12 @@ const authorInput =  document.getElementById('author');
 const pagesInput = document.getElementById('pages');
 const yearInput = document.getElementById('year');
 const readChkBox = document.getElementById('read');
-const form = document.querySelector('form')
+const form = document.querySelector('form');
+
 
 if (myLibrary.length > 0) {
   let headings = Object.keys(myLibrary[0]);
+  headings.shift();
   createTable(table, myLibrary)
   createTableHead(table, headings);
 }
@@ -115,7 +144,16 @@ function resetForm(...inputs) {
 }
 
 function addBookToLibrary() {
+  let bookCount;
+  if (myLibrary.length === 0) {
+    bookCount = 0;
+  } else {
+    bookCount = myLibrary[myLibrary.length - 1].count;
+    bookCount = Number(bookCount) + 1;
+  }
+  
   let book = {
+    count: bookCount,
     title: titleInput.value,
     author: authorInput.value,
     pages: pagesInput.value,
@@ -135,6 +173,7 @@ addBook.addEventListener('click', e => {
   
   if (document.querySelector('thead') === null) {
     let headings = Object.keys(myLibrary[0]);
+    headings.shift();
     createTable(table, myLibrary)
     createTableHead(table, headings);
   } else {
@@ -143,5 +182,16 @@ addBook.addEventListener('click', e => {
 
   resetForm(titleInput, authorInput, pagesInput, yearInput, readChkBox);
   form.style.display = "none";
+});
+
+const deleteBook = document.querySelectorAll('.delete-book');
+
+deleteBook.forEach((button) => {
+  button.addEventListener('click', e => {
+    let bookNumber = (e.target.parentNode.parentNode.dataset.bookCount) - 1;
+    myLibrary.splice(bookNumber, 1);
+    console.log(myLibrary);
+    // remove book from myLibrary and from table using the data attribute
+  });
 });
 
